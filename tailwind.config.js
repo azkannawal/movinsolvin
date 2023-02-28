7970206b7190ad7ef5b2113840bae390aac4b0ec
azkannawal <azkannawalaf@gmail.com>
2023-02-28 22:16:57 +0700

    feb

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ['index.html', 'thanks.html'],
+  content: ['index.html', 'thanks.html', 'februari.html'],
   theme: {
     extend: {},
   },
